38e0ea83615b9cc1814a3de3be2d10fae57b4d20
cleverboss01 <ndubeansdaniel97@gmail.com>
2022-04-22 16:46:42 +0100

    react-state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+// import pic from "./profile.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullname: "Daniel Ndubuisi",
+      bio: "Music and React Lover",
+      profession: "Fullstack developer",
+      imgSrc: "./profile.jpg",
+    },
+    shows: true,
+  };
+
+  handleShow = () => this.setState({ person: { fullname: "James Bond" } });
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src={this.state.person.imgSrc}
+            alt="profile-pic"
+            style={{ borderRadius: "50%" }}
+          />
+          <h2>{this.state.person.fullname}</h2>
+          <h3>{this.state.person.bio}</h3>
+          <h4>{this.state.person.profession}</h4>
+          <button onClick={this.handleShow}>Show Profile</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
