9dacb3f9b2853f7efbfdabb8f452a1f0710b5b6e
cleverboss01 <ndubeansdaniel97@gmail.com>
2022-04-25 15:21:47 +0100

    react state checkpoint

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
 import React, { Component } from "react";
 import "./App.css";
-// import pic from "./profile.jpg";
+import pic from "./profile.jpg";
 
 class App extends Component {
   state = {
     person: {
       fullname: "Daniel Ndubuisi",
-      bio: "Music and React Lover",
+      bio: "Musician, React Developer & Creator",
       profession: "Fullstack developer",
       imgSrc: "./profile.jpg",
     },
+    count: 0,
     shows: true,
   };
 
-  handleShow = () => this.setState({ person: { fullname: "James Bond" } });
+  handleShow = () =>
+    this.setState((prevState) => ({ ...prevState, shows: !prevState.shows }));
+
+  // Component life cycle field
+  componentDidMount() {
+    const newIntervalId = setInterval(() => {
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          count: prevState.count + 1,
+        };
+      });
+    }, 1000);
+
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        intervalId: newIntervalId,
+      };
+    });
+  }
 
   render() {
+    const { imgSrc, fullname, bio, profession } = this.state.person;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img
-            src={this.state.person.imgSrc}
-            alt="profile-pic"
-            style={{ borderRadius: "50%" }}
-          />
-          <h2>{this.state.person.fullname}</h2>
-          <h3>{this.state.person.bio}</h3>
-          <h4>{this.state.person.profession}</h4>
-          <button onClick={this.handleShow}>Show Profile</button>
+          <img src={pic} alt="profile-pic" style={{ borderRadius: "50%" }} />
+
+          {this.state.shows && (
+            <div>
+              <img src={imgSrc} alt="" style={{ display: "none" }} />
+              <h2 style={{ color: "greenyellow" }}>{fullname}</h2>
+              <h3>{bio}</h3>
+              <h4>{profession}</h4>
+              <small>This user logged in {this.state.count} seconds ago</small>
+            </div>
+          )}
+          <button onClick={this.handleShow}>
+            {this.state.shows ? "Hide" : "Show"} Profile
+          </button>
         </header>
       </div>
     );
